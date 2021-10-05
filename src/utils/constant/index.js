8fc1d8725e161608365eed0d3bc5a33256108903
chaedirdwiantara <chaedirdwiantara@gmail.com>
2021-10-05 15:33:47 +0700

    add API RAJAONGKIR into project

--- src/utils/constant/index.js
+++ src/utils/constant/index.js
@@ -1,2 +1,9 @@
 export const heightMobileUI = 896;
 export const widthMobileUI = 414;
+
+export const API_KEY = 'bd1044a52b300991359211f400cfb543';
+export const API_RAJAONGKIR = 'https://api.rajaongkir.com/starter/';
+export const API_HEADER_RAJAONGKIR = {
+  key: API_KEY,
+};
+export const API_TIMEOUT = 120000;
