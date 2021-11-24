ce8bbf7f0e992966a5400b2e1698c678e8c70128
chaedirdwiantara <chaedirdwiantara@gmail.com>
2021-11-25 06:00:04 +0700

    finish change password, create action & reducer

--- src/actions/ProfileAction.js
+++ src/actions/ProfileAction.js
@@ -1,3 +1,4 @@
+import {Alert} from 'react-native';
 import FIREBASE from '../config/FIREBASE';
 import {
   dispatchError,
@@ -7,6 +8,7 @@ import {
 } from '../utils';
 
 export const UPDATE_PROFILE = 'UPDATE_PROFILE';
+export const CHANGE_PASSWORD = 'CHANGE_PASSWORD';
 
 export const updateProfile = data => {
   return dispatch => {
@@ -40,5 +42,36 @@ export const updateProfile = data => {
         dispatchError(dispatch, UPDATE_PROFILE, error.message);
         alert(error.message);
       });
+  };
+};
+
+export const changePassword = data => {
+  return dispatch => {
+    dispatchLoading(dispatch, CHANGE_PASSWORD);
+    //CEK DULU APAKAH BENDAR EMAIL & PASSWORD LAMA (LOGIN)
+    FIREBASE.auth()
+      .signInWithEmailAndPassword(data.email, data.password)
+      .then(response => {
+        //jika sukses maka update pass
+        const user = FIREBASE.auth().currentUser;
+
+        user
+          .updatePassword(data.newPassword)
+          .then(() => {
+            // Update successful.
+            dispatchSuccess(dispatch, CHANGE_PASSWORD, 'Sukses Ganti Password');
+          })
+          .catch(error => {
+            dispatchError(dispatch, CHANGE_PASSWORD, error);
+            alert(error);
+          });
+      })
+      .catch(error => {
+        // ERROR
+        dispatchError(dispatch, CHANGE_PASSWORD, error.message);
+        alert(error.message);
+      });
+
+    //JIKA SUKSES MAKA GANTI PASSWORD
   };
 };
--- src/pages/ChangePassword/index.js
+++ src/pages/ChangePassword/index.js
@@ -1,16 +1,87 @@
 import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Alert, StyleSheet, View} from 'react-native';
 import {Inputan, Tombol} from '../../components';
-import {colors, responsiveHeight} from '../../utils';
+import {colors, getData, responsiveHeight} from '../../utils';
+import {connect} from 'react-redux';
+import {changePassword} from '../../actions/ProfileAction';
 
-export default class ChangePassword extends Component {
+class ChangePassword extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      password: '',
+      newPassword: '',
+      newPasswordConfirmation: '',
+    };
+  }
+
+  onSubmit = () => {
+    console.log('masuk');
+    const {password, newPassword, newPasswordConfirmation} = this.state;
+
+    if (newPassword !== newPasswordConfirmation) {
+      Alert.alert(
+        'Error',
+        'Password Baru dan Konfirmasi Password Baru Harus Sama',
+      );
+    } else if (password && newPassword && newPasswordConfirmation) {
+      //ambil data email dari local storage
+      getData('user').then(res => {
+        const parameter = {
+          email: res.email,
+          password: password,
+          newPassword: newPassword,
+        };
+
+        this.props.dispatch(changePassword(parameter));
+      });
+    } else {
+      Alert.alert(
+        'Error',
+        'Password Lama, Password Baru dan Konfirmasi Password Baru Harus Diisi',
+      );
+    }
+  };
+
+  componentDidUpdate(prevProps) {
+    const {changePasswordResult} = this.props;
+
+    if (
+      changePasswordResult &&
+      prevProps.changePasswordResult !== changePasswordResult
+    ) {
+      Alert.alert('Sukses', 'Sukses ganti Password');
+      this.props.navigation.replace('MainApp');
+    }
+  }
+
   render() {
+    const {password, newPassword, newPasswordConfirmation} = this.state;
+    const {changePasswordLoading} = this.props;
     return (
       <View style={styles.pages}>
         <View>
-          <Inputan label="Password Lama" secureTextEntry />
-          <Inputan label="Password Baru" secureTextEntry />
-          <Inputan label="Konfirmasi Password Baru" secureTextEntry />
+          <Inputan
+            label="Password Lama"
+            secureTextEntry
+            value={password}
+            onChangeText={password => this.setState({password})}
+          />
+          <Inputan
+            label="Password Baru"
+            secureTextEntry
+            value={newPassword}
+            onChangeText={newPassword => this.setState({newPassword})}
+          />
+          <Inputan
+            label="Konfirmasi Password Baru"
+            secureTextEntry
+            value={newPasswordConfirmation}
+            onChangeText={newPasswordConfirmation =>
+              this.setState({newPasswordConfirmation})
+            }
+          />
         </View>
 
         <View style={styles.submit}>
@@ -20,12 +91,22 @@ export default class ChangePassword extends Component {
             icon="submit"
             padding={responsiveHeight(15)}
             fontSize={18}
+            onPress={() => this.onSubmit()}
+            loading={changePasswordLoading}
           />
         </View>
       </View>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  changePasswordLoading: state.ProfileReducer.changePasswordLoading,
+  changePasswordResult: state.ProfileReducer.changePasswordResult,
+  changePasswordError: state.ProfileReducer.changePasswordError,
+});
+
+export default connect(mapStateToProps, null)(ChangePassword);
 
 const styles = StyleSheet.create({
   pages: {
